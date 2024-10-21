f013fb3bad9aa0f54529f59fc784fe838bc041e5
tox-in <habimanat78@gmail.com>
2024-10-21 21:14:29 +0200

    simple dashboard

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    path('dashboard/', include('dashboard.urls')),
     path('items/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

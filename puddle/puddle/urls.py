5f680313e40af9718fa6756c3cc34de1498c22dc
tox-in <habimanat78@gmail.com>
2024-10-21 14:49:50 +0200

    signing up

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -2,12 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index
-from core.views import contact
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
-    path('', index, name="index"),
-    path('contact/', contact, name="contact"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
